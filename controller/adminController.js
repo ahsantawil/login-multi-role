e6d5fdb4d8b646b0677a64c17022015a537a52a3
ahsantawil <ahsan.tawil07@gmail.com>
2020-09-09 08:02:26 +0700

    add view all directory

--- controller/adminController.js
+++ controller/adminController.js
@@ -8,5 +8,47 @@ module.exports = {
     },
     addPengiriman : (req, res) => {
         res.render('admin/pengiriman/addPengiriman');
-    }    
+    },
+
+    viewAnalisa : (req, res) => {
+        res.render('admin/analisa/viewAnalis');
+    },
+
+    viewCeksn   : (req, res) => {
+        res.render('admin/ceksn/viewCeksn');
+    },
+
+    viewConsumers : (req, res) => {
+        res.render('admin/consumers/viewConsumers');
+    },
+    viewFis       : (req, res) => {
+        res.render('admin/fis/viewFis');
+    },
+    viewKlaim     : (req, res) => {
+        res.render('admin/klaim/viewKlaim');
+    },
+    viewDocument  : (req, res) => {
+        res.render('admin/klaim/viewDocument');
+    },
+    addKlaim      : (req, res) => {
+        res.render('admin/klaim/addKlaim');
+    },
+    viewPenggantian : (req, res) => {
+        res.render('admin/penggantian/viewPenggantian');
+    },
+    addPenggantian : (req, res) => {
+        res.render('admin/penggantian/addPenggantian');
+    },
+    viewProduct : (req, res) => {
+        res.render('admin/product/viewProduct');
+    },
+    viewReport  : (req, res) => {
+        res.render('admin/report/viewReport');
+    },
+    viewSerach  : (req, res) => {
+        res.render('admin/searchsn/viewSearch');
+    },
+    viewUsers   : (req, res) => {
+        res.render('admin/users/viewUsers');
+    }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -8,5 +8,38 @@ router.get('/dashboard', adminController.viewDashboard);
 router.get('/pegiriman', adminController.addPengiriman);
 router.get('/report-pengiriman', adminController.viewPengiriman);
 
+// end point analisa
+router.get('/analis', adminController.viewAnalisa);
+
+//end point ceksn
+router.get('/ceksn', adminController.viewCeksn);
+
+//end point consumers
+router.get('/consumers', adminController.viewConsumers);
+
+// end point fis
+router.get('/fis', adminController.viewFis);
+
+// end point klaim
+router.get('/klaim', adminController.addKlaim);
+router.get('/document', adminController.viewDocument);
+router.get('/report-klaim', adminController.viewKlaim);
+
+//end point penggantian
+router.get('/penggantian', adminController.addPenggantian);
+router.get('/report-penggantian', adminController.viewPenggantian);
+
+// end point product
+router.get('/product', adminController.viewProduct);
+
+// end point report
+router.get('/report', adminController.viewProduct);
+
+// end point serachsn
+router.get('/searchsn', adminController.viewSerach);
+
+// end point users
+router.get('/users', adminController.viewUsers);
+
 
 module.exports = router;
