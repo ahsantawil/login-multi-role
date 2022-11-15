74f6531af8691e1b7162c61a1e55c967e7e0682d
ahsantawil <ahsan.tawil07@gmail.com>
2022-11-15 16:54:17 +0700

    create component, pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div>
-      lorem ipsum 
+        <BrowserRouter>
+          <Routes>
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
