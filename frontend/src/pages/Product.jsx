e2b189cfb33e66b5a2176fba3f20b92c0ce09d4a
ahsantawil <ahsan.tawil07@gmail.com>
2022-11-18 16:24:01 +0700

    components, routing, pages , axios default credentials, and authSlice

---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+import Productlist from '../components/Productlist';
+
+const Product = () => {
+  return (
+    <Layout>
+        <Productlist />
+    </Layout>
+  )
+}
+
+export default Product;
