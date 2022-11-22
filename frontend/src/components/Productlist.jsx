0eaf8e45ab9357f2cb30b5654c5dbcdcfb06f79b
ahsantawil <ahsan.tawil07@gmail.com>
2022-11-22 17:05:12 +0700

    axios page product

--- frontend/src/components/Productlist.jsx
+++ frontend/src/components/Productlist.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { IoAddCircle, IoTrash, IoCreate } from 'react-icons/io5';
 
 const Productlist = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = async () => {
+        const response = await axios('http://localhost:5000/products')
+        setProducts(response.data);
+    };
+
+    const deleteProduct = async (productId) => {
+        await axios.delete(`http://localhost:5000/products/${productId}`);
+        getProducts();
+    };
+
   return (
     <div>
         <h1 className='title'>Product</h1>
         <h1 className='subtitle'>List Of Product</h1>
+        <Link to={'/products/add'} className='button is-primary mb-2'><IoAddCircle /></Link>
         <table className="table is-striped is-fullwidth">
             <thead>
                 <tr>
@@ -16,13 +36,18 @@ const Productlist = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                {products.map((product, index)=> (
+                <tr key={product.uuid}>
+                    <td>{index + 1}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.user.name}</td>
+                    <td>
+                        <Link to={`/products/edit/${product.uuid}`} className='button is-small is-info mr-2'><IoCreate /></Link>
+                        <button onClick={()=> deleteProduct(product.uuid)} className='button is-small is-danger'><IoTrash/></button>
+                    </td>
                 </tr>
+                ))}
             </tbody>
         </table>
     </div>
--- frontend/src/components/Userlist.jsx
+++ frontend/src/components/Userlist.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { IoAddCircle, IoTrash, IoCreate } from 'react-icons/io5';
 
 const Userlist = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = async () => {
+        const response = await axios('http://localhost:5000/users')
+        setUsers(response.data);
+    };
+
+    const deleteUsers = async (userId) => {
+        await axios.delete(`http://localhost:5000/users/${userId}`);
+        getUsers();
+    };
+
   return (
     <div>
         <h1 className='title'>Users</h1>
         <h1 className='subtitle'>List Of Users</h1>
+        <Link to={'/users/add'} className='button is-primary mb-2'><IoAddCircle /></Link>
         <table className="table is-striped is-fullwidth">
             <thead>
                 <tr>
@@ -16,13 +36,18 @@ const Userlist = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                {users.map((user, index) => (
+                <tr key={user.uuid}>
+                    <td>{index + 1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                    <td>
+                        <Link to={`/users/edit/${user.uuid}`} className='button is-small is-info mr-2'><IoCreate/></Link>
+                        <button onClick={() => deleteUsers(user.uuid)} className='button is-small is-danger'><IoTrash/></button>
+                    </td>
                 </tr>
+                ))}
             </tbody>
         </table>
     </div>
